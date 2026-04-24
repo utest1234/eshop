b7e11d9946487c703a449e2859e9e922b2a4f55d
unknown <dsd647283@gmail.com>
2026-04-24 10:22:28 +0800

    Initial Django + Supabase setup

--- eshop/settings.py
+++ eshop/settings.py
@@ -139,5 +139,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- store/models.py
+++ store/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Customer(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True,max_length=100)
+    password=models.CharField(max_length=128)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    description=models.TextField(blank=True,max_length=500,default="",null=True)
+    image=models.ImageField(upload_to='uploads/product/', blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    order_date = models.DateTimeField(default=datetime.datetime.now)
+    
+    address=models.CharField(max_length=255, default="",blank=True)
+    phone=models.CharField(max_length=8, default="",blank=True)
+    status=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
